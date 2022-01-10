e1a6264ec6021b4fae549f7d19e2942c186394a6
Marcos Gois <marcosvagois@outlook.com>
2022-01-10 02:35:56 -0300

    Include BLE Function and Click Button to Scan

--- BLE_Wifi_Manager/BLE_Wifi_Manager.ino
+++ BLE_Wifi_Manager/BLE_Wifi_Manager.ino
@@ -1,48 +1,83 @@
-/*
- *  This sketch demonstrates how to scan WiFi networks.
- *  The API is almost the same as with the WiFi Shield library,
- *  the most obvious difference being the different file you need to include:
- */
+#include "SimpleBLE.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
 #include "WiFi.h"
+
+SimpleBLE ble;
+
+void onButton() {
+  String out = "BLE32 name: ";
+  out += String(millis() / 1000);
+  Serial.println(out);
+  ble.begin(out);
+}
+
+
+void wifiScan() {
+  String out = "";
+  Serial.println("scan start");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+    out = "no networks found";
+    Serial.println("no networks found");
+  } else {
+    out += String(n);
+    out += " networks found\n";
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*");
+      delay(10);
+      out += String(i+1);
+      out += ":";
+      out += String(WiFi.SSID(i));
+      out += " (";
+      out += String(WiFi.RSSI(i));
+      out += ")";
+      (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? out += "\n" : out += "*\n";
+    }
+  }
+  Serial.println("");
+  Serial.println(out);
+}
 
 void setup()
 {
-    Serial.begin(115200);
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  pinMode(0, INPUT_PULLUP);
+  Serial.print("ESP32 SDK: ");
+  Serial.println(ESP.getSdkVersion());
+  ble.begin("Reuk Tec IOT");
+  Serial.println("Press the button to scan the WIFI's name");
+  // Set WiFi to station mode and disconnect from an AP if it was previously connected
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  delay(100);
 
-    // Set WiFi to station mode and disconnect from an AP if it was previously connected
-    WiFi.mode(WIFI_STA);
-    WiFi.disconnect();
-    delay(100);
-
-    Serial.println("Setup done");
+  Serial.println("Setup done");
 }
 
 void loop()
 {
-    Serial.println("scan start");
-
-    // WiFi.scanNetworks will return the number of networks found
-    int n = WiFi.scanNetworks();
-    Serial.println("scan done");
-    if (n == 0) {
-        Serial.println("no networks found");
-    } else {
-        Serial.print(n);
-        Serial.println(" networks found");
-        for (int i = 0; i < n; ++i) {
-            // Print SSID and RSSI for each network found
-            Serial.print(i + 1);
-            Serial.print(": ");
-            Serial.print(WiFi.SSID(i));
-            Serial.print(" (");
-            Serial.print(WiFi.RSSI(i));
-            Serial.print(")");
-            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
-            delay(10);
-        }
-    }
-    Serial.println("");
-
-    // Wait a bit before scanning again
-    delay(5000);
+  static uint8_t lastPinState = 1;
+  uint8_t pinState = digitalRead(0);
+  if (!pinState && lastPinState) {
+    wifiScan();
+  }
+  lastPinState = pinState;
+  while (Serial.available()) Serial.write(Serial.read());
 }
